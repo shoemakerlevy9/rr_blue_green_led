d12c179afff79b0c110e17f9d7dcd452106a77a5
Ubuntu <ubuntu@ip-172-31-27-216.us-west-2.compute.internal>
2019-05-30 20:23:07 +0000

    clarifying led parameters

--- scripts/test_pub.py
+++ scripts/test_pub.py
@@ -2,6 +2,13 @@
 
 import rospy
 from std_msgs.msg import Int16MultiArray
+form enum import Enum
+
+color = {
+    'blue': 0,
+    'green': 1,
+    
+}
 
 def talker():
     pub = rospy.Publisher('led_control', Int16MultiArray, queue_size=10)
@@ -9,7 +16,11 @@ def talker():
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
         msg = Int16MultiArray()
-        msg.data = [2,200]
+        
+        # Edit here to change the LED intesity: values between 0 - 255
+        msg.data[color.blue] = 0     # Edit this line to set intensity of Blue light
+        msg.data[color.green] = 255  # Edit this line to set Intesity of Green Light
+        
         pub.publish(msg)
         rate.sleep()
 
